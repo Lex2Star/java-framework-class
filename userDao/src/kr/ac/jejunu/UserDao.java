61ce8738d39e49bb75c52c648f930be281f823f6
Lex2Star <hyunki@hyunki.local>
2017-04-21 11:13:30 +0900

    Factory Method Pattern

--- userDao/src/kr/ac/jejunu/UserDao.java
+++ userDao/src/kr/ac/jejunu/UserDao.java
@@ -5,12 +5,8 @@ import java.sql.*;
 /**
  * Created by hyunki on 2017. 3. 15..
  */
-public class UserDao {
-
-    private Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        return DriverManager.getConnection("jdbc:mysql://localhost/jeju?characterEncoding=utf-8", "jeju", "jejupw");
-    }
+public abstract class UserDao {
+    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
 
     public User get(Long id) throws SQLException, ClassNotFoundException {
         Connection connection = getConnection();
