eddef86c643443f318635f7d53598b20197e6da2
Lex2Star <hyunki@hyunki.local>
2017-04-21 14:20:57 +0900

    Spring

--- userDao/src/kr/ac/jejunu/DaoFactory.java
+++ userDao/src/kr/ac/jejunu/DaoFactory.java
@@ -1,15 +1,19 @@
 package kr.ac.jejunu;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by hyunki on 2017. 4. 21..
  */
+@Configuration
 public class DaoFactory {
-
-    public UserDao getUserDao() {
+    @Bean
+    public UserDao userDao() {
         return new UserDao(getConnection());
     }
-
-    private ConnectionMaker getConnection() {
+    @Bean
+    public ConnectionMaker getConnection() {
         return new JejuUserDao();
     }
 }
